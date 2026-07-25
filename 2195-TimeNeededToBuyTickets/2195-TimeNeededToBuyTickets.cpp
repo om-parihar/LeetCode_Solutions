fb6d07b134f57614ff0f8b63165180c5c9b5f236
Om Parihar <omshivoham191@gmail.com>
2026-07-25 22:56:49 +0530

    Create 2195-TimeNeededToBuyTickets

---
+++ 2195-TimeNeededToBuyTickets/2195-TimeNeededToBuyTickets.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/25/2026, 10:56:47 PM
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int n=tickets.size();
+        int t=0;
+        for(int i=0;i<n;i++){
+            if(i<=k){
+                t+=min(tickets[i],tickets[k]);
+            }
+            else{
+                t+=min(tickets[i],tickets[k]-1);
+            }
+        }
+        return t;
+    }
+};
