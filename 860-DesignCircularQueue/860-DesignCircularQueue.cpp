27f6b6a7ccd16c8579b6d2e2f8b40a2d7bd6137d
Om Parihar <omshivoham191@gmail.com>
2026-07-25 22:58:34 +0530

    Create 860-DesignCircularQueue

---
+++ 860-DesignCircularQueue/860-DesignCircularQueue.cpp
@@ -0,0 +1,51 @@
+// Last updated: 7/25/2026, 10:58:32 PM
+class MyCircularQueue {
+public:
+    vector<int> q;
+    int front, rear, count, size;
+
+    MyCircularQueue(int k) {
+        size = k;
+        q.resize(k);
+        front = 0;
+        rear = -1;
+        count = 0;
+    }
+
+    bool enQueue(int value) {
+        if (isFull()) return false;
+
+        rear = (rear + 1) % size;
+        q[rear] = value;
+        count++;
+
+        return true;
+    }
+
+    bool deQueue() {
+        if (isEmpty()) return false;
+
+        front = (front + 1) % size;
+        count--;
+
+        return true;
+    }
+
+    int Front() {
+        if (isEmpty()) return -1;
+        return q[front];
+    }
+
+    int Rear() {
+        if (isEmpty()) return -1;
+        return q[rear];
+    }
+
+    bool isEmpty() {
+        return count == 0;
+    }
+
+    bool isFull() {
+        return count == size;
+    }
+};
