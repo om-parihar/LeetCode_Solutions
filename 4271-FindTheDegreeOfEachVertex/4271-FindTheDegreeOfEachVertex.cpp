ae72b549f57854212d2b5482fab20784372b4795
Om Parihar <omshivoham191@gmail.com>
2026-07-25 22:56:04 +0530

    Create 4271-FindTheDegreeOfEachVertex

---
+++ 4271-FindTheDegreeOfEachVertex/4271-FindTheDegreeOfEachVertex.cpp
@@ -0,0 +1,14 @@
+// Last updated: 7/25/2026, 10:56:02 PM
+class Solution {
+public:
+    vector<int> findDegrees(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        vector<int> ans(n,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                ans[i]+=matrix[i][j];
+            }
+        }
+        return ans;
+    }
+};
