d34be1c3cab178227cf6ba63e43f0e815ca9336f
Om Parihar <omshivoham191@gmail.com>
2026-07-25 22:57:50 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.cpp
@@ -0,0 +1,12 @@
+// Last updated: 7/25/2026, 10:57:48 PM
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if(n%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
