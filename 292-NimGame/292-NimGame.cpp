1a6c323f175109111d3a90b3d1b8cb4679983fb1
Om Parihar <omshivoham191@gmail.com>
2026-07-25 23:00:12 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.cpp
@@ -0,0 +1,12 @@
+// Last updated: 7/25/2026, 11:00:10 PM
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n%4==0){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+};
