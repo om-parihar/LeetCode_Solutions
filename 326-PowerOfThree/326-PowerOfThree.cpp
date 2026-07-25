d2cf98eca06c9596448572f1ba2150f787a46670
Om Parihar <omshivoham191@gmail.com>
2026-07-25 23:00:09 +0530

    Create 326-PowerOfThree

---
+++ 326-PowerOfThree/326-PowerOfThree.cpp
@@ -0,0 +1,13 @@
+// Last updated: 7/25/2026, 11:00:07 PM
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n%3==0){
+            n=n/3;
+        }
+        return n==1;
+    }
+};
