da3b0a7ccc3703fccbbefd29940a6f7a7ea4b09d
Om Parihar <omshivoham191@gmail.com>
2026-07-25 23:00:00 +0530

    Create 349-IntersectionOfTwoArrays

---
+++ 349-IntersectionOfTwoArrays/349-IntersectionOfTwoArrays.cpp
@@ -0,0 +1,28 @@
+// Last updated: 7/25/2026, 10:59:58 PM
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        vector<int> res;
+        int i=0,j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(i>0 && nums1[i]==nums1[i-1]){
+                i++;
+                continue;
+            }
+            if(nums1[i]==nums2[j]){
+                res.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        return res;
+    }
+};
