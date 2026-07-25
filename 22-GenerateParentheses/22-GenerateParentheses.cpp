143036dc74a888b40d37cbf98d5eed06224de148
Om Parihar <omshivoham191@gmail.com>
2026-07-25 23:03:08 +0530

    Create 22-GenerateParentheses

---
+++ 22-GenerateParentheses/22-GenerateParentheses.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/25/2026, 11:03:06 PM
+class Solution {
+public:
+    void help(int n,int left, int right, vector<string> &ans,string &temp){
+        if(left+right==2*n){
+            ans.push_back(temp);
+            return;
+        }
+        if(left<n){
+            temp.push_back('(');
+            help(n,left+1,right,ans,temp);
+            temp.pop_back();
+        }
+        if(right<left){
+            temp.push_back(')');
+            help(n,left,right+1,ans,temp);
+            temp.pop_back();
+        }
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string temp;
+        help(n,0,0,ans,temp);
+        return ans;
+    }
+};
