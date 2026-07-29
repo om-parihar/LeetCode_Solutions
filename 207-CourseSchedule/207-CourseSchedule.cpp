402e4424b983dfb1dd6d1528871bc54d2c3113b1
Om Parihar <omshivoham191@gmail.com>
2026-07-29 23:29:12 +0530

    File updated at 7/29/2026, 11:29:05 PM

--- 207-CourseSchedule/207-CourseSchedule.cpp
+++ 207-CourseSchedule/207-CourseSchedule.cpp
@@ -1,5 +1,5 @@
-// Last updated: 7/29/2026, 11:22:21 PM
-1class Solution {
+// Last updated: 7/29/2026, 11:29:05 PM
+1class Solution1 {
 2public:
 3    bool check(int node, vector<vector<int>> &edges, vector<int> &vis, vector<int> &path){
 4        vis[node]=1;
@@ -30,3 +30,42 @@
 29        return true;
 30    }
 31};
+32
+33
+34class Solution {
+35public:
+36    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+37        vector<int> indegree(numCourses,0);
+38        vector<vector<int>> edges(numCourses);
+39        for(int i=0;i<prerequisites.size();i++){
+40            int u=prerequisites[i][0];
+41            int v=prerequisites[i][1];
+42            edges[v].push_back(u);
+43        }
+44        queue<int> q;
+45        vector<int> topo;
+46        for(int i=0;i<numCourses;i++){
+47            for(auto it:edges[i]){
+48                indegree[it]++;
+49            }
+50        }
+51        for(int i=0;i<numCourses;i++){
+52            if(indegree[i]==0){
+53                q.push(i);
+54            }
+55        }
+56        while(!q.empty()){
+57            int a=q.front();
+58            q.pop();
+59            topo.push_back(a);
+60            for(auto it:edges[a]){
+61                indegree[it]--;
+62                if(indegree[it]==0){
+63                    q.push(it);
+64                }
+65            }
+66        }
+67        if(topo.size()==numCourses) return true;
+68        return false;
+69    }
+70};
