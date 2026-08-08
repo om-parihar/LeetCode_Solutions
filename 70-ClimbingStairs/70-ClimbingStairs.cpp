11d91ce0bc2061806835a645e837f6ede74e7153
Om Parihar <omshivoham191@gmail.com>
2026-08-08 12:06:13 +0530

    File updated at 8/8/2026, 12:06:10 PM

--- 70-ClimbingStairs/70-ClimbingStairs.cpp
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -1,14 +1,15 @@
-// Last updated: 7/25/2026, 11:02:25 PM
-class Solution {
-public:
-    int climbStairs(int n) {
-       if(n<=1) return 1;
-       int prev=1,prev2=1;
-       for(int i=2;i<=n;i++){
-            int curr=prev+prev2;
-            prev2=prev;
-            prev=curr;
-       }
-       return prev;
-    }
-};
+// Last updated: 8/8/2026, 12:06:10 PM
+1class Solution {
+2public:
+3    int climb(int n,vector<int> &dp){
+4        if(n==0) return 1;
+5        if(n==1) return 1;
+6        if(dp[n]!=-1) return dp[n];
+7        return dp[n]=climb(n-1,dp)+climb(n-2,dp);
+8    }
+9    int climbStairs(int n) {
+10        vector<int> dp(n+1,-1);
+11        int cnt=climb(n,dp);
+12        return cnt;
+13    }
+14};
