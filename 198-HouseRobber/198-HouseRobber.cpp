1ed12401e9f1f5cf429533b2cabf4a341aabc03f
Om Parihar <omshivoham191@gmail.com>
2026-08-08 15:47:55 +0530

    File updated at 8/8/2026, 3:47:52 PM

--- 198-HouseRobber/198-HouseRobber.cpp
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -1,5 +1,5 @@
-// Last updated: 8/8/2026, 3:23:37 PM
-1class Solution {
+// Last updated: 8/8/2026, 3:47:52 PM
+1class Solution1 {
 2public:
 3    int fun(int idx, vector<int> nums,vector<int> &dp){
 4        if(idx==0) return nums[idx];
@@ -16,3 +16,20 @@
 15        return cnt;
 16    }
 17};
+18
+19class Solution {
+20public:
+21    int rob(vector<int>& nums) {
+22        int n=nums.size();
+23        vector<int> dp(n+1);
+24        if(n==1) return nums[0];
+25        dp[0]=nums[0];
+26        dp[1]=max(nums[0],nums[1]);
+27        for(int i=2;i<n;i++){
+28            int take=nums[i]+dp[i-2];
+29            int notake=0+dp[i-1];
+30            dp[i]=max(take,notake);
+31        }
+32        return dp[n-1];
+33    }
+34};
