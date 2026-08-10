fe082c7a6070c44cb7b75b11cc9f88da729c3947
Om Parihar <omshivoham191@gmail.com>
2026-08-10 16:54:31 +0530

    File updated at 8/10/2026, 4:54:31 PM

--- 64-MinimumPathSum/64-MinimumPathSum.cpp
+++ 64-MinimumPathSum/64-MinimumPathSum.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/10/2026, 4:49:48 PM
+// Last updated: 8/10/2026, 4:54:31 PM
 1class Solution1 {
 2public:
 3    int count(int i, int j, vector<vector<int>> &grid){
@@ -16,7 +16,7 @@
 15    }
 16};
 17
-18class Solution {
+18class Solution2 {
 19public:
 20    int count(int i, int j, vector<vector<int>> &grid,vector<vector<int>> &dp){
 21        if(i==0 && j==0) return grid[0][0];
@@ -34,3 +34,27 @@
 33        return cnt;
 34    }
 35};
+36
+37
+38
+39class Solution {
+40public:
+41    int minPathSum(vector<vector<int>>& grid) {
+42        int n=grid.size();
+43        int m=grid[0].size();
+44        vector<vector<int>> dp(n,vector<int>(m,-1));
+45        for(int i=0;i<n;i++){
+46            for(int j=0;j<m;j++){
+47                if(i==0 && j==0){
+48                    dp[i][j]=grid[i][j];
+49                    continue;
+50                }
+51                int up=1e9,left=1e9;
+52                if(i>0) up=dp[i-1][j];
+53                if(j>0) left=dp[i][j-1];
+54                dp[i][j]=grid[i][j]+min(up,left);
+55            }
+56        }
+57        return dp[n-1][m-1];
+58    }
+59};
