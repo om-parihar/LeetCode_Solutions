e64ae250b17c0171a5ee33ad72c16a3d2a6c5239
Om Parihar <omshivoham191@gmail.com>
2026-08-14 15:03:20 +0530

    File updated at 8/14/2026, 3:03:19 PM

--- 455-AssignCookies/455-AssignCookies.cpp
+++ 455-AssignCookies/455-AssignCookies.cpp
@@ -1,21 +1,21 @@
-// Last updated: 7/25/2026, 10:59:29 PM
-class Solution {
-public:
-    int findContentChildren(vector<int>& g, vector<int>& s) {
-        sort(g.begin(),g.end());
-        sort(s.begin(),s.end());
-        int a=g.size();
-        int b=s.size();
-        int sum=0;
-        int i=0;
-        int j=0;
-        while(i<a && j<b){
-            if(g[i]<=s[j]){
-                sum++;
-                i++;
-            }
-            j++;
-        }
-        return sum;
-    }
-};
+// Last updated: 8/14/2026, 3:03:19 PM
+1class Solution {
+2public:
+3    int findContentChildren(vector<int>& g, vector<int>& s) {
+4        sort(g.begin(),g.end());
+5        sort(s.begin(),s.end());
+6        int a=g.size();
+7        int b=s.size();
+8        int sum=0;
+9        int i=0;
+10        int j=0;
+11        while(i<a && j<b){
+12            if(g[i]<=s[j]){
+13                sum++;
+14                i++;
+15            }
+16            j++;
+17        }
+18        return sum;
+19    }
+20};
