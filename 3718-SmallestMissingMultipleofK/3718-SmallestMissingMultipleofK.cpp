1de2b301986a96fecfae03ceac56462f4a701e4f
Om Parihar <omshivoham191@gmail.com>
2026-08-25 21:24:04 +0530

    File updated at 8/25/2026, 9:23:56 PM

--- 3718-SmallestMissingMultipleofK/3718-SmallestMissingMultipleofK.cpp
+++ 3718-SmallestMissingMultipleofK/3718-SmallestMissingMultipleofK.cpp
@@ -1,21 +1,33 @@
-// Last updated: 8/25/2026, 9:20:59 PM
+// Last updated: 8/25/2026, 9:23:56 PM
 1class Solution {
 2public:
 3    int missingMultiple(vector<int>& nums, int k) {
 4        int ans=k;
-5        while(1){
-6            int flg=0;
-7            for(auto it: nums){
-8                if(it==ans){
-9                    flg=1;
-10                    break;
-11                }
-12            }
-13            if(flg==0){
-14                break;
-15            }
-16            ans+=k;
-17        }
-18        return ans;
-19    }
-20};
+5        unordered_set<int> st(nums.begin(),nums.end());
+6        while(st.count(ans)){
+7            ans+=k;
+8        }
+9        return ans;
+10    }
+11};
+12
+13class Solution1 {
+14public:
+15    int missingMultiple(vector<int>& nums, int k) {
+16        int ans=k;
+17        while(1){
+18            int flg=0;
+19            for(auto it: nums){
+20                if(it==ans){
+21                    flg=1;
+22                    break;
+23                }
+24            }
+25            if(flg==0){
+26                break;
+27            }
+28            ans+=k;
+29        }
+30        return ans;
+31    }
+32};
